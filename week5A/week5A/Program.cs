e1111cc6fca92a2b01ff1e8874b0f20663b7f5ea
cdm-star <colin0503@icloud.com>
2024-09-24 09:04:50 -0400

    buildingblock with adding numbers

---
+++ week5A/week5A/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace week5A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Nick");
+            int myFavNum = 3;
+            int theBestNum = 4;
+
+            int sum;
+            sum = myFavNum + theBestNum;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
